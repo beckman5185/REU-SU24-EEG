3d2cc058c2c0fa5bf31b39010a7d0dfff3846c3b
victornorton <110307502+victornorton@users.noreply.github.com>
2024-07-03 12:12:16 -0400

    Update imaginaryCoherenceTest.py

--- analysis/imaginaryCoherenceTest.py
+++ analysis/imaginaryCoherenceTest.py
@@ -15,22 +15,29 @@ def main():
     Ch1 = pd.Series(EEGdata[0])
     Ch9 = pd.Series(EEGdata[8])
 
+    numSegs = 1024
     #Finds imaginary component. 
     #Taken from: https://dsp.stackexchange.com/questions/61661/how-to-compare-imaginary-coherence-values
-    f1, Pxx = scipy.signal.welch(Ch1, fs=500, nperseg = 30000)
-    f2, Pyy = scipy.signal.welch(Ch1, fs=500, nperseg = 30000)
-    f3, Pxy = scipy.signal.csd(Ch1, Ch9, fs=500, nperseg = 30000)
-    Icoh = np.imag(Pxy)/np.sqrt(Pxx*Pyy)
+    f1, Pxx = scipy.signal.welch(Ch1, fs=500, nperseg = numSegs )
+    f2, Pyy = scipy.signal.welch(Ch1, fs=500, nperseg = numSegs )
+    f3, Pxy = scipy.signal.csd(Ch1, Ch9, fs=500, nperseg = numSegs)
+    Icoh = np.abs(np.imag(Pxy))/np.sqrt(Pxx*Pyy)
+    Coh = np.abs((Pxy))/np.sqrt(Pxx*Pyy)
+
+    xAxis = np.arange(0, 500 + (500/(numSegs/2)), (500/(numSegs/2)))
 
     #print(Icoh)
 
+    plt.xlim(0,60)
     #Plots resulting array
-    plt.plot(Icoh)
+    plt.plot(xAxis, Icoh, Coh)
     plt.show()
 
+    plt.xlim(0,60)
+    plt.plot(xAxis, Coh)
+    plt.show()
 
-
-    
+   
 
 
 if __name__ == "__main__":
